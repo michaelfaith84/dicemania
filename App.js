723adead34213740d788d0049ffebb33d97b2e5c
michael <michael.p.faith@gmail.com>
2022-02-10 13:51:28 -0800

    Initial commit.
    Operates by using a "die pool." Supported dice are: d4, d6, d8, d10, d20.

--- App.js
+++ App.js
@@ -1,20 +1,164 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import * as eva from "@eva-design/eva";
+import {
+  ApplicationProvider,
+  Layout,
+  Text,
+  Button,
+  IndexPath,
+} from "@ui-kitten/components";
+import DieSelector from "./components/DieSelector";
+import randomInteger from "random-int";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import {
+  faCoffee,
+  faDiceD4,
+  faDiceD6,
+  faDiceD8,
+  faDiceD10,
+  faDiceD12,
+  faDiceD20,
+} from "@fortawesome/pro-solid-svg-icons";
 
-export default function App() {
+const HomeScreen = () => {
+  // Die Type
+  const [selectedIndex, setSelectedIndex] = React.useState(new IndexPath(0));
+  // Die Quantity
+  const [dieQty, setDieQty] = React.useState(0);
+  const data = ["d4", "d6", "d8", "d10", "d12", "d20"];
+  const iconMap = {
+    d4: faDiceD4,
+    d6: faDiceD6,
+    d8: faDiceD8,
+    d10: faDiceD10,
+    d12: faDiceD12,
+    d20: faDiceD20,
+  };
+  const [diePool, setDiePool] = useState([]);
+  const [results, setResults] = useState([]);
+  const addDie = () => {
+    const dice = [];
+    const resultsArr = [];
+    for (let i = 0; i < dieQty; i++) {
+      dice.push(data[selectedIndex.row]);
+    }
+    diePool.length > 0 ? setDiePool([...diePool, ...dice]) : setDiePool(dice);
+    // diePool.map((d) => resultsArr.push(icon(d)));
+    // setResults(resultsArr);
+  };
+  const icon = (die) => {
+    return (
+      <Text>
+        <FontAwesomeIcon
+          style={{ padding: 10, flexWrap: "wrap" }}
+          size={32}
+          icon={iconMap[die]}
+        />
+      </Text>
+    );
+  };
+  const iconResult = (die) => {
+    return (
+      <Text>
+        <FontAwesomeIcon
+          style={{ padding: 10, flexWrap: "wrap" }}
+          size={32}
+          icon={iconMap[die]}
+        />
+        {randomInteger(1, parseInt(die.replace(/[^\d]/g, "")))}
+      </Text>
+    );
+  };
+  const rollDice = () => {
+    let results = [];
+    if (diePool.length > 0) {
+      diePool.map((die) => {
+        results.push(iconResult(die));
+      });
+      setResults(results);
+      console.log(results);
+    }
+  };
+  const resetDiePool = () => {
+    if (diePool.length > 0) {
+      setDiePool([]);
+      setResults([]);
+    }
+  };
+
+  useEffect(() => {
+    if (diePool.length > 0) {
+      const icons = [];
+      diePool.map((die) => icons.push(icon(die)));
+      setResults(icons);
+    }
+  }, [diePool]);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Layout
+      style={{
+        flex: 1,
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 25,
+        paddingTop: 50,
+      }}
+      level={"4"}
+    >
+      <Layout
+        style={{
+          flexDirection: "row",
+          justifyContent: "flex-start",
+          marginBottom: 25,
+        }}
+      >
+        <DieSelector
+          props={{
+            selectedIndex,
+            setSelectedIndex,
+            dieQty,
+            setDieQty,
+            data,
+            addDie,
+          }}
+        />
+      </Layout>
+      <Layout
+        style={{
+          flex: 1,
+          flexDirection: "row",
+          flexWrap: "wrap",
+          justifyContent: "flex-start",
+          width: "100%",
+        }}
+      >
+        {results.length > 0 ? results.map((res) => res) : null}
+      </Layout>
+      <Layout
+        style={{
+          width: "100%",
+          marginTop: 25,
+          justifyContent: "flex-end",
+        }}
+      >
+        <Button onPress={rollDice}>Roll!</Button>
+      </Layout>
+      <Layout
+        style={{
+          width: "100%",
+          marginTop: 25,
+          justifyContent: "flex-end",
+        }}
+      >
+        <Button onPress={resetDiePool}>Reset Pool!</Button>
+      </Layout>
+    </Layout>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default () => (
+  <ApplicationProvider {...eva} theme={eva.light}>
+    <HomeScreen />
+  </ApplicationProvider>
+);
---
+++ components/DieSelector.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect, Fragment } from "react";
+import {
+  Select,
+  SelectItem,
+  IndexPath,
+  Layout,
+  Input,
+  Button,
+} from "@ui-kitten/components";
+
+const DieSelector = ({
+  props: { selectedIndex, setSelectedIndex, dieQty, setDieQty, data, addDie },
+}) => {
+  // useEffect(() => {
+  //   // console.log(data[selectedIndex.row]);
+  // }, [selectedIndex]);
+  // const [selectedIndex, setSelectedIndex] = React.useState(new IndexPath(0));
+  // const addDie = () => {
+  //   const dice = [];
+  //   for (let i = 0; i <= dieQty; i++) {
+  //     dice.push(data[selectedIndex.row]);
+  //   }
+  // };
+  const displayValue = data[selectedIndex.row];
+  const renderOption = (title, index) => (
+    <SelectItem style={{ alignContent: "center" }} title={title} key={index} />
+  );
+
+  const onDieQtyChange = (qty) => {
+    setDieQty(qty.replace(/[^\d]/g, ""));
+  };
+
+  return (
+    <Layout
+      style={{
+        flex: 1,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+      }}
+    >
+      <Layout style={{ flex: 1 }}>
+        <Select
+          // style={styles.select}
+          style={{ flex: 1, height: "100%" }}
+          value={displayValue}
+          selectedIndex={selectedIndex}
+          onSelect={(index) => setSelectedIndex(index)}
+        >
+          {data.map(renderOption)}
+        </Select>
+      </Layout>
+      <Layout style={{ flex: 1 }}>
+        <Input
+          placeholder={"Quantity"}
+          value={dieQty}
+          onChangeText={(qty) => onDieQtyChange(qty)}
+        />
+      </Layout>
+      <Layout style={{ flexShrink: 1 }}>
+        <Button onPress={addDie}>+</Button>
+      </Layout>
+    </Layout>
+  );
+};
+
+export default DieSelector;
